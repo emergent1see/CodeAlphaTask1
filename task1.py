739ca4ec3d00cbc39bfd6d5b934ac3bf91a5f72f
Atta Ur Rehman <70438981+emergent1see@users.noreply.github.com>
2025-03-25 22:20:03 +0500

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,20 @@
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
+
+n = int(input("Enter the number of terms: "))
+fibonacci(n)
+
+def fibonacci_recursive(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
+
+n = int(input("Enter the number of terms: "))
+for i in range(n):
+    print(fibonacci_recursive(i), end=" ")
